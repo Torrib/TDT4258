d7d6a9f11e3daf5218acc6dbd3ac21c05cc2caba
Per Øyvind Kanestrøm <perokane@gmail.com>
2014-01-24 15:29:42 +0100

    Reset handler

---
+++ oving1/oving1.asm
@@ -0,0 +1,12 @@
+/∗  Place the following from address 0 in memory ∗/
+.long  0x1000
+
+/∗  Initial stack pointer ∗/
+.long reset
+
+/∗  Address of reset handler ∗/
+/∗  if required, other handler addresses follows here ∗/
+.thumb func
+
+reset :
+/∗  program code here ∗/
