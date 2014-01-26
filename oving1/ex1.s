ad852772f15ae95d098adbdeb06ce523a9bf6685
Per Øyvind Kanestrøm <perokane@gmail.com>
2014-01-26 13:33:21 +0100

    Enabled GPIO

--- oving1/ex1.s
+++ oving1/ex1.s
@@ -1,104 +1,117 @@
         .syntax unified
-	
-	      .include "efm32gg.s"
 
-	/////////////////////////////////////////////////////////////////////////////
-	//
-  // Exception vector table
-  // This table contains addresses for all exception handlers
-	//
-	/////////////////////////////////////////////////////////////////////////////
-	
+          .include "efm32gg.s"
+
+    /////////////////////////////////////////////////////////////////////////////
+    //
+    // Exception vector table
+    // This table contains addresses for all exception handlers
+    //
+    /////////////////////////////////////////////////////////////////////////////
+
         .section .vectors
-	
-	      .long   stack_top               /* Top of Stack                 */
-	      .long   _reset                  /* Reset Handler                */
-	      .long   dummy_handler           /* NMI Handler                  */
-	      .long   dummy_handler           /* Hard Fault Handler           */
-	      .long   dummy_handler           /* MPU Fault Handler            */
-	      .long   dummy_handler           /* Bus Fault Handler            */
-	      .long   dummy_handler           /* Usage Fault Handler          */
-	      .long   dummy_handler           /* Reserved                     */
-	      .long   dummy_handler           /* Reserved                     */
-	      .long   dummy_handler           /* Reserved                     */
-	      .long   dummy_handler           /* Reserved                     */
-	      .long   dummy_handler           /* SVCall Handler               */
-	      .long   dummy_handler           /* Debug Monitor Handler        */
-	      .long   dummy_handler           /* Reserved                     */
-	      .long   dummy_handler           /* PendSV Handler               */
-	      .long   dummy_handler           /* SysTick Handler              */
 
-	      /* External Interrupts */
-	      .long   dummy_handler
-	      .long   gpio_handler            /* GPIO even handler */
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   gpio_handler            /* GPIO odd handler */
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
-	      .long   dummy_handler
+          .long   stack_top               /* Top of Stack                 */
+          .long   _reset                  /* Reset Handler                */
+          .long   dummy_handler           /* NMI Handler                  */
+          .long   dummy_handler           /* Hard Fault Handler           */
+          .long   dummy_handler           /* MPU Fault Handler            */
+          .long   dummy_handler           /* Bus Fault Handler            */
+          .long   dummy_handler           /* Usage Fault Handler          */
+          .long   dummy_handler           /* Reserved                     */
+          .long   dummy_handler           /* Reserved                     */
+          .long   dummy_handler           /* Reserved                     */
+          .long   dummy_handler           /* Reserved                     */
+          .long   dummy_handler           /* SVCall Handler               */
+          .long   dummy_handler           /* Debug Monitor Handler        */
+          .long   dummy_handler           /* Reserved                     */
+          .long   dummy_handler           /* PendSV Handler               */
+          .long   dummy_handler           /* SysTick Handler              */
 
-	      .section .text
+          /* External Interrupts */
+          .long   dummy_handler
+          .long   gpio_handler            /* GPIO even handler */
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   gpio_handler            /* GPIO odd handler */
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
+          .long   dummy_handler
 
-	/////////////////////////////////////////////////////////////////////////////
-	//
-	// Reset handler
+          .section .text
+
+    /////////////////////////////////////////////////////////////////////////////
+    //
+    // Reset handler
   // The CPU will start executing here after a reset
-	//
-	/////////////////////////////////////////////////////////////////////////////
+    //
+    /////////////////////////////////////////////////////////////////////////////
+    cmu_base_addr:
+        .long CMU_BASE
 
-	      .globl  _reset
-	      .type   _reset, %function
+          .globl  _reset
+          .type   _reset, %function
         .thumb_func
-_reset: 
-	      b .  // do nothing
-	
-	/////////////////////////////////////////////////////////////////////////////
-	//
+_reset:
+    ldr r1, cmu_base_addr //Load CMU base adress
+    ldr r2, [r1, #CMU_HFPERCLKEN0] //Load current value of HFPERCLK_ENABLE
+
+    //Set GPIO clock
+    mov r3, #1
+    lsl r3, r3, #CMU_HFPERCLKEN0_GPIO
+
+    orr r2, r2, r3
+
+    //store new value
+    str r2, [r1, #CMU_HFPERCLKEN0]
+
+          b .  // do nothing
+
+    /////////////////////////////////////////////////////////////////////////////
+    //
   // GPIO handler
   // The CPU will jump here when there is a GPIO interrupt
-	//
-	/////////////////////////////////////////////////////////////////////////////
-	
-        .thumb_func
-gpio_handler:  
+    //
+    /////////////////////////////////////////////////////////////////////////////
 
-	      b .  // do nothing
-	
-	/////////////////////////////////////////////////////////////////////////////
-	
         .thumb_func
-dummy_handler:  
+gpio_handler:
+
+          b .  // do nothing
+
+    /////////////////////////////////////////////////////////////////////////////
+
+        .thumb_func
+dummy_handler:
         b .  // do nothing
-
