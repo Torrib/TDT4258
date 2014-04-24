8e8aa68b4d9eed5ee5fc4a99830d52cc5cb97927
lindkjop <pialindkjolen@gmail.com>
2014-04-24 21:24:24 +0200

    fixed the board - still not finished

--- oving3/oving3-xcode/oving3-xcode/main.c
+++ oving3/oving3-xcode/oving3-xcode/main.c
@@ -21,28 +21,54 @@ int main(int argc, const char * argv[])
     int column, row, x, y;
     char board[3][3];
     
+    //Should be in a method
     for (x = 0; x < 3; x++) {
+        
         for (y = 0; y < 3; y++) {
-            //For some reason the values on the board is "J" instead of "." as they should be. And if you add that the middle one should be "P" The values are "R" and the middle one "T"
-            board[x][y] = ".";
-            board[1][1] = "P";
+            board[x][y] = '.';
         }
+
     }
-    
+    //Should be a method printBoard()
     for (int i = 0; i < 3; i++) {
         for (int x = 0; x < 3; x++) {
             printf("%c", board[i][x]);
-            
         }
         printf("\n");
     }
     
-    while (hasTurn != 0) {
-        printf("Player 1 - Choose column 0 - 2 :");
+    while (hasTurn == 1) {
+        printf("Player 1 - Choose column 0 - 2 :\n");
         scanf("%d", &column);
-        printf("Player 1 - Choose row 0 - 2 :");
+        printf("Player 1 - Choose row 0 - 2 :\n");
         scanf("%d", &row);
         
+        //Check if the column and row is allowed
+        //Should be in a method
+        if(column > 2
+           || column < 0
+           || row > 2
+           || row < 0) {
+            printf("Column or row not valid - try again.\n");
+        }
+        //Set the right field to "X"
+        board[column][row] = 'X';
+        
+        //Print the new board
+        for (int i = 0; i < 3; i++) {
+            for (int x = 0; x < 3; x++) {
+                printf("%c", board[i][x]);
+            }
+            printf("\n");
+        }
+        //Check if player 1 has won, if not - switch to next player. This is ugly, need fixing
+        //Check rows
+        for (x = 0; x < 3; x++) {
+            if (board[]) {
+                <#statements#>
+            }
+        }
+    
     }
     return 0;
 }
