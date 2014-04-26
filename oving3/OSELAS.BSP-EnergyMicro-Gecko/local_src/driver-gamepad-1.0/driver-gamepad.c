bb66b31b33257fc6550cb03c35ad7ac0d3766384
Torrib <torrib90@gmail.com>
2014-04-26 16:58:11 +0200

    Gamepad driver

--- oving3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
+++ oving3/OSELAS.BSP-EnergyMicro-Gecko/local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -1,41 +1,183 @@
-/*
- * This is a demo Linux kernel module.
- */
 
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/moduleparam.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/ioport.h>
+#include <linux/types.h>
+#include <asm/io.h>
+#include <asm/uaccess.h>
+#include <linux/signal.h>
+#include <linux/interrupt.h>
+#include <asm/siginfo.h>	//siginfo
+#include <linux/rcupdate.h>	//rcu_read_lock
+#include <linux/sched.h>	//find_task_by_pid_type
 
-/*
- * template_init - function to insert this module into kernel space
- *
- * This is the first of two exported functions to handle inserting this
- * code into a running kernel
- *
- * Returns 0 if successfull, otherwise -1
- */
+#include "efm32gg.h"
 
-static int __init template_init(void)
+#define NAME "Gamepad Driver"
+
+typedef unsigned int uint32_t;
+typedef unsigned short uint16_t;
+typedef unsigned char uint8_t;
+
+//Prototypes
+static int __init driver_init(void);
+static void __exit driver_exit(void);
+static int driver_open(struct inode *inode, struct file *filep);
+static int driver_release(struct inode *inode, struct file *filep);
+static ssize_t driver_read(struct file *filep, char __user *buff, size_t count, loff_t *offp);
+static ssize_t driver_write(struct file *filep, const char __user *buff, size_t count, loff_t *offp);
+void write_register(uint32_t offset, uint32_t value);
+uint32_t read_register(uint32_t offset);
+static irqreturn_t irq_handler(int irq, void *dummy, struct pt_regs * regs);
+
+//File operation functions
+static struct file_operations driver_fops = {
+	.owner = THIS_MODULE,
+	.read = driver_read,
+	.write = driver_write,
+	.open = driver_open,
+	.release = driver_release
+};
+
+//This driver
+static struct cdev driver_cdev = {};
+
+//Device number
+dev_t devicenumber = 0;
+
+//GPIO memory pointer
+void __iomem *gpio;
+
+char output;
+struct task_struct *t;
+static int __init driver_init(void)
 {
-	printk("Hello World, here is your module speaking\n");
+	//Get device number
+	alloc_chrdev_region(&devicenumber, 0, 1, NAME);
+
+	//Request and map memory
+	check_mem_region(GPIO_BASE, GPIO_SIZE);
+	request_mem_region(GPIO_BASE, GPIO_SIZE, NAME);
+
+	gpio = ioremap_nocache(GPIO_BASE, GPIO_SIZE);
+
+	//Set up GPIO
+	write_register(GPIO_PA_CTRL, 0x2);
+	write_register(GPIO_PA_MODEH, 0x55555555);
+	write_register(GPIO_PA_DOUT, 0xFF00);
+	write_register(GPIO_PC_MODEL, 0x33333333);
+	write_register(GPIO_PC_DOUT, 0xFF);
+	write_register(GPIO_IEN, 0xFF);
+	write_register(GPIO_EXTIPSELL, 0x22222222);
+	write_register(GPIO_EXTIRISE, 0xFF);
+	write_register(GPIO_EXTIFALL, 0xFF);
+	write_register(GPIO_IFC, 0xFFFF);
+
+	request_irq(17, irq_handler, 0, NAME, NULL);
+	request_irq(18, irq_handler, 0, NAME, NULL);
+
+	//Register driver
+	cdev_init(&driver_cdev, &driver_fops);
+	driver_cdev.owner = THIS_MODULE;
+	cdev_add(&driver_cdev, devicenumber, 1);
+
+	printk(KERN_INFO "%s loaded... Major number is %d\n", NAME, MAJOR(devicenumber));
+
 	return 0;
 }
 
-/*
- * template_cleanup - function to cleanup this module from kernel space
- *
- * This is the second of two exported functions to handle cleanup this
- * code from a running kernel
- */
-
-static void __exit template_cleanup(void)
+static void __exit driver_exit(void)
 {
-	 printk("Short life for a small module...\n");
+	cdev_del(&driver_cdev);
+	iounmap(gpio);
+	release_mem_region(GPIO_BASE, GPIO_SIZE);
+	unregister_chrdev_region(devicenumber, 1);
+	free_irq(17, NULL);
+	free_irq(18, NULL);
+	printk(KERN_INFO "%s unloaded...", NAME);
 }
 
-module_init(template_init);
-module_exit(template_cleanup);
+static int driver_open (struct inode *inode, struct file *filp)
+{
+	printk(KERN_INFO "%s opened\n", NAME);
+	return 0;
+}
 
-MODULE_DESCRIPTION("Small module, demo only, not very useful.");
+static int driver_release (struct inode *inode, struct file *filp)
+{
+	printk(KERN_INFO "%s closed\n", NAME);
+	return 0;
+}
+
+static ssize_t driver_read (struct file *filp, char __user *buff, size_t count, loff_t *offp)
+{
+	//Return button state
+	if (count == 0)
+		return 0;
+	copy_to_user(buff, &output, 1);
+	return 1;
+}
+
+static ssize_t driver_write (struct file *filp, const char __user *buff, size_t count, loff_t *offp)
+{
+	char pid_string[10];
+	int pid = 0;
+	int ret;
+
+	/* read the value from user space */
+	if(count > 10)
+		return -EINVAL;
+	copy_from_user(pid_string, buf, count);
+	sscanf(pid_string, "%d", &pid);
+
+	rcu_read_lock();
+	t = find_task_by_pid_type(PIDTYPE_PID, pid);  //find the task_struct associated with this pid
+	if(t == NULL){
+		printk("no such pid\n");
+		rcu_read_unlock();
+		return -ENODEV;
+	}
+	rcu_read_unlock();
+	return count;
+}
+
+void write_register(uint32_t offset, uint32_t value)
+{
+	*(volatile uint32_t *) ((uint32_t) gpio + offset) = value;
+}
+
+uint32_t read_register(uint32_t offset)
+{
+	return *(volatile uint32_t *) ((uint32_t) gpio + offset); 
+}
+
+static irqreturn_t irq_handler(int irq, void *dev_id, struct pt_regs * regs)
+{
+	uint32_t buttons = read_register(GPIO_PC_DIN);
+	struct siginfo info;
+	output = (uint8_t) ~buttons;
+	/* send the signal */
+	memset(&info, 0, sizeof(struct siginfo));
+	info.si_signo = 42;
+	info.si_code = SI_QUEUE;	
+	info.si_int = output;
+	ret = send_sig_info(42, &info, t);
+	if (ret < 0) {
+		printk("error sending signal\n");
+		return ret;
+	}
+
+	write_register(GPIO_IFC, 0xFFFF);
+	return IRQ_HANDLED;
+}
+
+module_init(driver_init);
+module_exit(driver_exit);
+
+MODULE_DESCRIPTION("Simple driver for TDT4258 Gamepad");
 MODULE_LICENSE("GPL");
-
--- oving3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
+++ oving3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
@@ -1,9 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <linux/fb.h>
+#include <sys/mman.h>
+
+int framebuffer;
+int gamepad;
 
 int main(int argc, char *argv[])
 {
-	printf("Hello World, I'm game!\n");
 
-	exit(EXIT_SUCCESS);
+	//Open the framebuffer file
+	framebuffer = open("/dev/fb0", O_RDWR);
+
+	if(framebuffer < 0)
+	{
+		printf("Error opening frame buffer\n");
+		return 1;
+	}
+
+	//Map the framebuffer to the memory
+	if(map_file(&framebuffer, fd_framebuffer, FB_SIZE) != 0)
+	{
+		printf("Error: Mapping buffer to memory");
+		return 1;
+	}
+
+	gamepad = open("/dev/gamepad", O_RDWR);
+	if(gamepad < 0) 
+	{
+		printf("Error: opening gamepad file\n");
+		return 1;
+	}
+
+	game_stuff();
+
+
+}
+
+void game_stuff()
+{
+	memset(framebuffer, 0x0000, FB_SIZE);
+
+	struct fb_copyarea rect;
+
+	rect.x = 100;
+	rect.y = 100;
+	rect.width = 30;
+	rect.height = 30;
+
+	ioctl(framebuffer, 0x4680, &rect);
 }
