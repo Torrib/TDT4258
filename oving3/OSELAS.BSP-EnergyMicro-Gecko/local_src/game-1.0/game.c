84df309250b32f3b7d5f3e2a2992355ea096d315
Torrib <torrib90@gmail.com>
2014-04-26 19:20:10 +0200

    sfasdf

--- oving3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
+++ oving3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
@@ -3,6 +3,8 @@
 #include <linux/fb.h>
 #include <sys/mman.h>
 #include <signal.h>
+#include <sys/stat.h>
+#include <stdint.h>
 
 int framebuffer;
 int gamepad;
@@ -35,18 +37,18 @@ int main(int argc, char *argv[])
 	uint16_t *screen;
 	screen = (uint16_t *) mmap(NULL, 320*240*2, PROT_READ | PROT_WRITE, MAP_SHARED, framebuffer, 0);
 
-	if(screen == null)
+	if(screen == NULL)
 		return 1;
 
 	struct sigaction sign;
-	sign.sa_sigaction = button_handler;
+	sign.sa_sigaction = interrupt_handler;
 	sign.sa_flags = SA_SIGINFO;
 	sigaction(5, &sign, NULL);
 
 
-	while(true)
+	while(1)
 	{
-		
+
 	}
 
 	//game_stuff();
@@ -59,14 +61,14 @@ void interrupt_handler(int n, siginfo_t *info, void *unused) {
 
 void game_stuff(void)
 {
-	memset(framebuffer, 0x0000, FB_SIZE);
+	//memset(framebuffer, 0x0000, FB_SIZE);
 
-	struct fb_copyarea rect;
+	// struct fb_copyarea rect;
 
-	rect.x = 100;
-	rect.y = 100;
-	rect.width = 30;
-	rect.height = 30;
+	// rect.x = 100;
+	// rect.y = 100;
+	// rect.width = 30;
+	// rect.height = 30;
 
-	ioctl(framebuffer, 0x4680, &rect);
+	// ioctl(framebuffer, 0x4680, &rect);
 }
