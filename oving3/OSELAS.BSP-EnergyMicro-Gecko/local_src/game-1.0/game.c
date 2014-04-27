a3a646d5edb8957678684f52534b0d1b8a054394
Torrib <torrib90@gmail.com>
2014-04-28 01:41:27 +0200

    asd

--- oving3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
+++ oving3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
@@ -142,10 +142,10 @@ int init_tictactoe()
     }
 
     //Run while the game has not finished
-    // while (running == 1) 
-    // {
-    //     sleep(1000);
-    // }
+    while (running == 1) 
+    {
+        sleep(1000);
+    }
 
     return 0;
 }
