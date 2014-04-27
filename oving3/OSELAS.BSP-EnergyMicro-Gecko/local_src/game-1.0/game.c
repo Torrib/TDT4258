4bc10ab6ebef27ec2c3054ee53403a9e7934a94c
Per Øyvind Kanestrøm <perokane@gmail.com>
2014-04-27 12:41:16 +0200

    Indent fix

--- oving3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
+++ oving3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.c
@@ -27,59 +27,59 @@ void tictactoe_event(int event);
 
 int main(int argc, char *argv[])
 {
-	printf("Starting game\n");
+    printf("Starting game\n");
     
-	char pid_buf[10];
+    char pid_buf[10];
     
-	//Open the framebuffer file
-	framebuffer = open("/dev/fb0", O_RDWR);
+    //Open the framebuffer file
+    framebuffer = open("/dev/fb0", O_RDWR);
     
-	//Exit if the framebuffer file does not exist or cant be opened.
-	if(framebuffer < 0)
-	{
-		printf("Error opening frame buffer\n");
-		return 1;
-	}
+    //Exit if the framebuffer file does not exist or cant be opened.
+    if(framebuffer < 0)
+    {
+        printf("Error opening frame buffer\n");
+        return 1;
+    }
     
-	gamepad = open("/dev/gamepad", O_RDWR);
-	if(gamepad < 0)
-	{
-		printf("Error: opening gamepad file\n");
-		return 1;
-	}
+    gamepad = open("/dev/gamepad", O_RDWR);
+    if(gamepad < 0)
+    {
+        printf("Error: opening gamepad file\n");
+        return 1;
+    }
     
-	//Initiate the screen
-	uint16_t *screen;
-	screen = (uint16_t *) mmap(NULL, 320*240*2, PROT_READ | PROT_WRITE, MAP_SHARED, framebuffer, 0);
+    //Initiate the screen
+    uint16_t *screen;
+    screen = (uint16_t *) mmap(NULL, 320*240*2, PROT_READ | PROT_WRITE, MAP_SHARED, framebuffer, 0);
     
-	if(screen == NULL)
-		return 1;
+    if(screen == NULL)
+        return 1;
     
-	struct sigaction sign;
-	sign.sa_sigaction = interrupt_handler;
-	sign.sa_flags = SA_SIGINFO;
-	sigaction(42, &sign, NULL);
+    struct sigaction sign;
+    sign.sa_sigaction = interrupt_handler;
+    sign.sa_flags = SA_SIGINFO;
+    sigaction(42, &sign, NULL);
     
-	sprintf(pid_buf, "%d", getpid());
-	write(gamepad, pid_buf, strlen(pid_buf) +1);
+    sprintf(pid_buf, "%d", getpid());
+    write(gamepad, pid_buf, strlen(pid_buf) +1);
     
     // The game begins!
     init_tictactoe();
 /*
     for(int i = 0; i < 10; i++)
-    	for(int y = 0; y < 50; y++)
-    		framebuffer[i * 320 + y] = 0xFFFF;
+        for(int y = 0; y < 50; y++)
+            framebuffer[i * 320 + y] = 0xFFFF;
   */  
-	while(1)
-	{
+    while(1)
+    {
         
-	}
+    }
     
-	//game_stuff();
+    //game_stuff();
 }
 
 void interrupt_handler(int n, siginfo_t *info, void *unused) {
-	uint8_t buttons = (uint8_t) ~(info->si_int);
+    uint8_t buttons = (uint8_t) ~(info->si_int);
     
     //Propagate the event here. [0 4] Up, down, left, right, click
     tictactoe_event(buttons);
@@ -93,7 +93,7 @@ void interrupt_handler(int n, siginfo_t *info, void *unused) {
 int init_tictactoe()
 {
     
-	printf("Game initializing");
+    printf("Game initializing");
     
     //hasTurn is 1 when it is player 1, and 2 when it is player two
     int hasTurn = 1;
